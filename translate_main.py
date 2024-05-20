3f8cdfbe33fce1f1875f6d566c2ac6e0c2caf13c
Ezequiel Marques de Oliveira <ezequielmarquesdeoliveira20@hotmail.com>
2024-05-20 14:18:08 -0300

    Feito esquema de threads.

--- translate_main.py
+++ translate_main.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 from googletrans import Translator
 
 
@@ -12,28 +13,44 @@ def translate_text(text, dest_lang):
         return "Ops...!:", "Erro na chamada da API!"
 
 
+def handle_client(client_socket, client_address):
+    try:
+        with client_socket:
+            print('Conexão de', client_address)
+            data = client_socket.recv(1024).decode('utf-8')
+            data = data.split('|')
+            if len(data) == 2:
+                text, dest_lang = data
+                translated_text = translate_text(text, dest_lang)
+                client_socket.sendall(translated_text.encode('utf-8'))
+            else:
+                client_socket.sendall("Requisição inválida!".encode('utf-8'))
+    except ConnectionResetError:
+        print(f"Conexão perdida com {client_address}")
+    finally:
+        client_socket.close()
+        print(f"Conexão encerrada com {client_address}")
+
+
 def main():
     host = '0.0.0.0'
     port = 12345
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((host, port))
-        server_socket.listen()
+        server_socket.listen(8)
 
         print("Servidor ouvindo na porta", port)
 
-        while True:
-            client_socket, address = server_socket.accept()
-            with client_socket:
-                print('Conexão de', address)
-                data = client_socket.recv(1024).decode('utf-8')
-                data = data.split('|')
-                if len(data) == 2:
-                    text, dest_lang = data
-                    translated_text = translate_text(text, dest_lang)
-                    client_socket.sendall(translated_text.encode('utf-8'))
-                else:
-                    client_socket.sendall("Requisição inválida!".encode('utf-8'))
+        try:
+            while True:
+                client_socket, client_address = server_socket.accept()
+                client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+                client_thread.start()
+        except KeyboardInterrupt:
+            print("Servidor encerrado")
+        finally:
+            server_socket.close()
 
 
 if __name__ == "__main__":
